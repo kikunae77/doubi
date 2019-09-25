4d90c110402e2df456c8c4e529080f34cec20e83
kikunae77 <54462749+kikunae77@users.noreply.github.com>
2019-09-25 14:22:50 +0800

    번역

--- ban_iptables.sh
+++ ban_iptables.sh
@@ -10,8 +10,8 @@ export PATH
 
 sh_ver="1.0.10"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Info="${Green_font_prefix}[정보]${Font_color_suffix}"
+Error="${Red_font_prefix}[오류]${Font_color_suffix}"
 
 smtp_port="25,26,465,587"
 pop3_port="109,110,995"
@@ -75,12 +75,12 @@ Cat_KEY_WORDS(){
 }
 View_PORT(){
 	Cat_PORT
-	echo -e "===============${Red_background_prefix} 当前已封禁 端口 ${Font_color_suffix}==============="
+	echo -e "===============${Red_background_prefix} 현재 사용 금지된 포트 ${Font_color_suffix}==============="
 	echo -e "$Ban_PORT_list" && echo && echo -e "==============================================="
 }
 View_KEY_WORDS(){
 	Cat_KEY_WORDS
-	echo -e "==============${Red_background_prefix} 当前已封禁 关键词 ${Font_color_suffix}=============="
+	echo -e "==============${Red_background_prefix} 현재 사용 금지된 키워드 ${Font_color_suffix}=============="
 	echo -e "$Ban_KEY_WORDS_list" && echo -e "==============================================="
 }
 View_ALL(){
@@ -172,19 +172,19 @@ Set_ALL(){
 }
 Ban_BT(){
 	check_BT
-	[[ ! -z ${BT_KEY_WORDS} ]] && echo -e "${Error} 检测到已封禁BT、PT 关键词，无需再次封禁 !" && exit 0
+	[[ ! -z ${BT_KEY_WORDS} ]] && echo -e "${Error} BT, PT 키워드 사용이 이미 금지되어 있습니다. 다시 금지할 필요가 없습니다 !" && exit 0
 	s="A"
 	Set_BT
 	View_ALL
-	echo -e "${Info} 已封禁BT、PT 关键词 !"
+	echo -e "${Info} BT,PT 키워드 사용을 금지하였습니다 !"
 }
 Ban_SPAM(){
 	check_SPAM
-	[[ ! -z ${SPAM_PORT} ]] && echo -e "${Error} 检测到已封禁SPAM(垃圾邮件) 端口，无需再次封禁 !" && exit 0
+	[[ ! -z ${SPAM_PORT} ]] && echo -e "${Error} SPAM(정크메일) 포트가 이미 사용 금지되어 있습니다. 다시 금지할 필요가 없습니다 !" && exit 0
 	s="A"
 	Set_SPAM
 	View_ALL
-	echo -e "${Info} 已封禁SPAM(垃圾邮件) 端口 !"
+	echo -e "${Info} SPAM(정크메일) 포트를 사용 금지하였습니다 !"
 }
 Ban_ALL(){
 	check_BT
@@ -194,37 +194,37 @@ Ban_ALL(){
 		if [[ -z ${SPAM_PORT} ]]; then
 			Set_ALL
 			View_ALL
-			echo -e "${Info} 已封禁BT、PT 关键词 和 SPAM(垃圾邮件) 端口 !"
+			echo -e "${Info} BT, PT 키워드 및 SPAM(정크메일) 포트 사용을 금지하였습니다 !"
 		else
 			Set_BT
 			View_ALL
-			echo -e "${Info} 已封禁BT、PT 关键词 !"
+			echo -e "${Info} BT, PT 키워드 사용을 금지하였습니다 !"
 		fi
 	else
 		if [[ -z ${SPAM_PORT} ]]; then
 			Set_SPAM
 			View_ALL
-			echo -e "${Info} 已封禁SPAM(垃圾邮件) 端口 !"
+			echo -e "${Info} SPAM(정크메일) 포트 사용을 금지하였습니다 !"
 		else
-			echo -e "${Error} 检测到已封禁BT、PT 关键词 和 SPAM(垃圾邮件) 端口，无需再次封禁 !" && exit 0
+			echo -e "${Error} BT, PT 키워드 및 SPAM(정크메일) 포트 사용이 이미 금지되어 있습니다. 다시 금지할 필요가 없습니다 !" && exit 0
 		fi
 	fi
 }
 UnBan_BT(){
 	check_BT
-	[[ -z ${BT_KEY_WORDS} ]] && echo -e "${Error} 检测到未封禁BT、PT 关键词，请检查 !" && exit 0
+	[[ -z ${BT_KEY_WORDS} ]] && echo -e "${Error} BT, PT 키워드 사용이 금지되어 있지 않습니다. 확인해주세요 !" && exit 0
 	s="D"
 	Set_BT
 	View_ALL
-	echo -e "${Info} 已解封BT、PT 关键词 !"
+	echo -e "${Info} BT, PT 키워드 사용 금지를 해제하였습니다 !"
 }
 UnBan_SPAM(){
 	check_SPAM
-	[[ -z ${SPAM_PORT} ]] && echo -e "${Error} 检测到未封禁SPAM(垃圾邮件) 端口，请检查 !" && exit 0
+	[[ -z ${SPAM_PORT} ]] && echo -e "${Error} SPAM(정크메일) 포트 사용이 금지되어 있지 않습니다. 확인해주세요 !" && exit 0
 	s="D"
 	Set_SPAM
 	View_ALL
-	echo -e "${Info} 已解封SPAM(垃圾邮件) 端口 !"
+	echo -e "${Info} SPAM(정크메일) 포트 사용 금지를 해제하였습니다 !"
 }
 UnBan_ALL(){
 	check_BT
@@ -234,19 +234,19 @@ UnBan_ALL(){
 		if [[ ! -z ${SPAM_PORT} ]]; then
 			Set_ALL
 			View_ALL
-			echo -e "${Info} 已解封BT、PT 关键词 和 SPAM(垃圾邮件) 端口 !"
+			echo -e "${Info} BT, PT 키워드 및 SPAM(정크메일) 포트 사용 금지를 해제하였습니다 !"
 		else
 			Set_BT
 			View_ALL
-			echo -e "${Info} 已解封BT、PT 关键词 !"
+			echo -e "${Info} BT, PT 키워드 사용 금지를 해제하였습니다 !"
 		fi
 	else
 		if [[ ! -z ${SPAM_PORT} ]]; then
 			Set_SPAM
 			View_ALL
-			echo -e "${Info} 已解封SPAM(垃圾邮件) 端口 !"
+			echo -e "${Info} SPAM(정크메일) 포트 사용 금지를 해제하였습니다 !"
 		else
-			echo -e "${Error} 检测到未封禁BT、PT 关键词和 SPAM(垃圾邮件) 端口，请检查 !" && exit 0
+			echo -e "${Error} BT, PT 키워드 및 SPAM(정크메일) 포트 사용 금지가 이미 해제되어 있습니다. 확인해주세요 !" && exit 0
 		fi
 	fi
 }
@@ -254,11 +254,11 @@ ENTER_Ban_KEY_WORDS_type(){
 	Type=$1
 	Type_1=$2
 	if [[ $Type_1 != "ban_1" ]]; then
-		echo -e "请选择输入类型：
- 1. 手动输入（只支持单个关键词）
- 2. 本地文件读取（支持批量读取关键词，每行一个关键词）
- 3. 网络地址读取（支持批量读取关键词，每行一个关键词）" && echo
-		read -e -p "(默认: 1. 手动输入):" key_word_type
+		echo -e "입력형식을 선택해주세요 :
+ 1. 수동입력 (한개의 키워드 입력만 지원)
+ 2. 로컬 파일 불러오기 (다수의 키워드 불러오기 지원, 매 행마다 1개의 키워드)
+ 3. 인터넷 주소에서 불러오기 (다수의 키워드 불러오기 지원, 매 행마다 1개의 키워드)" && echo
+		read -e -p "(기본값: 1. 수동입력):" key_word_type
 	fi
 	[[ -z "${key_word_type}" ]] && key_word_type="1"
 	if [[ ${key_word_type} == "1" ]]; then
@@ -280,68 +280,68 @@ ENTER_Ban_KEY_WORDS_type(){
 	fi
 }
 ENTER_Ban_PORT(){
-	echo -e "请输入欲封禁的 端口（单端口/多端口/连续端口段）"
+	echo -e "사용 금지할 포트를 입력하세요（단일포트/다중포트/연속된 포트범위）"
 	if [[ ${Ban_PORT_Type_1} != "1" ]]; then
-	echo -e "${Green_font_prefix}========示例说明========${Font_color_suffix}
- 单端口：25（单个端口）
- 多端口：25,26,465,587（多个端口用英文逗号分割）
- 连续端口段：25:587（25-587之间的所有端口）" && echo
+	echo -e "${Green_font_prefix}========입력방법========${Font_color_suffix}
+ 단일포트 : 25（포트 한개 입력）
+ 다중포트 : 25,26,465,587（여러개의 포트를 영문키보드의 ,로 분리하여 입력）
+ 연속된 포트범위 : 25:587（25-587사이의 모든 포트）" && echo
 	fi
-	read -e -p "(回车默认取消):" PORT
-	[[ -z "${PORT}" ]] && echo "已取消..." && View_ALL && exit 0
+	read -e -p "(엔터만 입력하면 취소됩니다):" PORT
+	[[ -z "${PORT}" ]] && echo "취소되었습니다..." && View_ALL && exit 0
 }
 ENTER_Ban_KEY_WORDS(){
-	echo -e "请输入欲封禁的 关键词（域名等，仅支持单个关键词）"
+	echo -e "사용 금지할 키워드를 입력하세요（도메인 등.. 한개의 키워드만 지원합니다.）"
 	if [[ ${Type_1} != "ban_1" ]]; then
-	echo -e "${Green_font_prefix}========示例说明========${Font_color_suffix}
- 关键词：youtube，即禁止访问任何包含关键词 youtube 的域名。
- 关键词：youtube.com，即禁止访问任何包含关键词 youtube.com 的域名（泛域名屏蔽）。
- 关键词：www.youtube.com，即禁止访问任何包含关键词 www.youtube.com 的域名（子域名屏蔽）。
- 更多效果自行测试（如关键词 .zip 即可禁止下载任何 .zip 后缀的文件）。" && echo
+	echo -e "${Green_font_prefix}========입력방법========${Font_color_suffix}
+ 키워드：youtube, youtube가 포함된 모든 도메인에 방문 금지.
+ 키워드：youtube.com, youtube.com이 포함된 모든 도메인에 방문 금지（최상위 도메인 제한）。
+ 키워드：www.youtube.com, www.youtube.com이 포함된 모든 도메인에 방문 금지（3차 도메인 제한）。
+ 다양한 키워드를 테스트해보세요. (키워드를 .zip 으로 설정하면 모든 .zip확장자를 가진 파일의 다운로드 금지)" && echo
 	fi
-	read -e -p "(回车默认取消):" key_word
-	[[ -z "${key_word}" ]] && echo "已取消..." && View_ALL && exit 0
+	read -e -p "(엔터만 입력하면 취소됩니다):" key_word
+	[[ -z "${key_word}" ]] && echo "취소되었습니다..." && View_ALL && exit 0
 }
 ENTER_Ban_KEY_WORDS_file(){
-	echo -e "请输入欲封禁/解封的 关键词本地文件（请使用绝对路径）" && echo
-	read -e -p "(默认 读取脚本同目录下的 key_word.txt ):" key_word
+	echo -e "사용 금지/금지 해제할 키워드 로컬 파일을 입력하세요. (절대경로로 입력)" && echo
+	read -e -p "(기본값은 스크립트와 동일한 디렉토리의 key_word.txt ):" key_word
 	[[ -z "${key_word}" ]] && key_word="key_word.txt"
 	if [[ -e "${key_word}" ]]; then
 		key_word=$(cat "${key_word}")
-		[[ -z ${key_word} ]] && echo -e "${Error} 文件内容为空 !" && View_ALL && exit 0
+		[[ -z ${key_word} ]] && echo -e "${Error} 파일 내용이 없습니다 !" && View_ALL && exit 0
 	else
-		echo -e "${Error} 没有找到文件 ${key_word} !" && View_ALL && exit 0
+		echo -e "${Error} 다음 파일을 찾을 수 없습니다. ${key_word} !" && View_ALL && exit 0
 	fi
 }
 ENTER_Ban_KEY_WORDS_url(){
-	echo -e "请输入欲封禁/解封的 关键词网络文件地址（例如 http://xxx.xx/key_word.txt）" && echo
-	read -e -p "(回车默认取消):" key_word
-	[[ -z "${key_word}" ]] && echo "已取消..." && View_ALL && exit 0
+	echo -e "사용 금지/금지 해제할 키워드 온라인 파일 주소를 입력하세요. (예. http://xxx.xx/key_word.txt)" && echo
+	read -e -p "(엔터만 입력하면 취소됩니다):" key_word
+	[[ -z "${key_word}" ]] && echo "취소되었습니다..." && View_ALL && exit 0
 	key_word=$(wget --no-check-certificate -t3 -T5 -qO- "${key_word}")
-	[[ -z ${key_word} ]] && echo -e "${Error} 网络文件内容为空或访问超时 !" && View_ALL && exit 0
+	[[ -z ${key_word} ]] && echo -e "${Error} 온라인 파일의 내용이 없거나 연결시간이 초과되었습니다 !" && View_ALL && exit 0
 }
 ENTER_UnBan_KEY_WORDS(){
 	View_KEY_WORDS
-	echo -e "请输入欲解封的 关键词（根据上面的列表输入完整准确的 关键词）" && echo
-	read -e -p "(回车默认取消):" key_word
-	[[ -z "${key_word}" ]] && echo "已取消..." && View_ALL && exit 0
+	echo -e "사용 금지를 해제할 키워드를 입력해주세요. (위 목록의 키워드와 완전히 동일한 키워드를 입력해주세요)" && echo
+	read -e -p "(엔터만 입력하면 취소됩니다):" key_word
+	[[ -z "${key_word}" ]] && echo "취소되었습니다..." && View_ALL && exit 0
 }
 ENTER_UnBan_PORT(){
-	echo -e "请输入欲解封的 端口（根据上面的列表输入完整准确的 端口，包括逗号、冒号）" && echo
-	read -e -p "(回车默认取消):" PORT
-	[[ -z "${PORT}" ]] && echo "已取消..." && View_ALL && exit 0
+	echo -e "사용 금지를 해제할 포트번호를 입력해주세요. (, 및 :를 포함하여 위 목록의 포트번호와 완전히 동일한 포트를 입력해주세요.)" && echo
+	read -e -p "(엔터만 입력하면 취소됩니다):" PORT
+	[[ -z "${PORT}" ]] && echo "취소되었습니다..." && View_ALL && exit 0
 }
 Ban_PORT(){
 	s="A"
 	ENTER_Ban_PORT
 	Set_PORT
-	echo -e "${Info} 已封禁端口 [ ${PORT} ] !\n"
+	echo -e "${Info} [ ${PORT} ] 포트를 사용 금지하였습니다 !\n"
 	Ban_PORT_Type_1="1"
 	while true
 	do
 		ENTER_Ban_PORT
 		Set_PORT
-		echo -e "${Info} 已封禁端口 [ ${PORT} ] !\n"
+		echo -e "${Info} [ ${PORT} ] 포트를 사용 금지하였습니다 !\n"
 	done
 	View_ALL
 }
@@ -349,52 +349,52 @@ Ban_KEY_WORDS(){
 	s="A"
 	ENTER_Ban_KEY_WORDS_type "ban"
 	Set_KEY_WORDS
-	echo -e "${Info} 已封禁关键词 [ ${key_word} ] !\n"
+	echo -e "${Info} [ ${key_word} ] 키워드를 사용 금지하였습니다 !\n"
 	while true
 	do
 		ENTER_Ban_KEY_WORDS_type "ban" "ban_1"
 		Set_KEY_WORDS
-		echo -e "${Info} 已封禁关键词 [ ${key_word} ] !\n"
+		echo -e "${Info} [ ${key_word} ] 키워드를 사용 금지하였습니다 !\n"
 	done
 	View_ALL
 }
 UnBan_PORT(){
 	s="D"
 	View_PORT
-	[[ -z ${Ban_PORT_list} ]] && echo -e "${Error} 检测到未封禁任何 端口 !" && exit 0
+	[[ -z ${Ban_PORT_list} ]] && echo -e "${Error} 사용 금지된 포트가 없습니다 !" && exit 0
 	ENTER_UnBan_PORT
 	Set_PORT
-	echo -e "${Info} 已解封端口 [ ${PORT} ] !\n"
+	echo -e "${Info} [ ${PORT} ] 포트를 사용 금지 해제하였습니다 !\n"
 	while true
 	do
 		View_PORT
-		[[ -z ${Ban_PORT_list} ]] && echo -e "${Error} 检测到未封禁任何 端口 !" && exit 0
+		[[ -z ${Ban_PORT_list} ]] && echo -e "${Error} 사용 금지된 포트가 없습니다 !" && exit 0
 		ENTER_UnBan_PORT
 		Set_PORT
-		echo -e "${Info} 已解封端口 [ ${PORT} ] !\n"
+		echo -e "${Info} [ ${PORT} ] 포트를 사용 금지 해제하였습니다 !\n"
 	done
 	View_ALL
 }
 UnBan_KEY_WORDS(){
 	s="D"
 	Cat_KEY_WORDS
-	[[ -z ${Ban_KEY_WORDS_list} ]] && echo -e "${Error} 检测到未封禁任何 关键词 !" && exit 0
+	[[ -z ${Ban_KEY_WORDS_list} ]] && echo -e "${Error} 사용 금지된 키워드가 없습니다 !" && exit 0
 	ENTER_Ban_KEY_WORDS_type "unban"
 	Set_KEY_WORDS
-	echo -e "${Info} 已解封关键词 [ ${key_word} ] !\n"
+	echo -e "${Info} [ ${key_word} ] 키워드를 사용 금지 해제하였습니다 !\n"
 	while true
 	do
 		Cat_KEY_WORDS
-		[[ -z ${Ban_KEY_WORDS_list} ]] && echo -e "${Error} 检测到未封禁任何 关键词 !" && exit 0
+		[[ -z ${Ban_KEY_WORDS_list} ]] && echo -e "${Error} 사용 금지된 키워드가 없습니다 !" && exit 0
 		ENTER_Ban_KEY_WORDS_type "unban" "ban_1"
 		Set_KEY_WORDS
-		echo -e "${Info} 已解封关键词 [ ${key_word} ] !\n"
+		echo -e "${Info} [ ${key_word} ] 키워드를 사용 금지 해제하였습니다 !\n"
 	done
 	View_ALL
 }
 UnBan_KEY_WORDS_ALL(){
 	Cat_KEY_WORDS
-	[[ -z ${Ban_KEY_WORDS_text} ]] && echo -e "${Error} 检测到未封禁任何 关键词，请检查 !" && exit 0
+	[[ -z ${Ban_KEY_WORDS_text} ]] && echo -e "${Error} 사용 금지된 키워드가 없습니다. 확인해주세요 !" && exit 0
 	if [[ ! -z "${v6iptables}" ]]; then
 		Ban_KEY_WORDS_v6_num=$(echo -e "${Ban_KEY_WORDS_v6_list}"|wc -l)
 		for((integer = 1; integer <= ${Ban_KEY_WORDS_v6_num}; integer++))
@@ -409,7 +409,7 @@ UnBan_KEY_WORDS_ALL(){
 	done
 	Save_iptables_v4_v6
 	View_ALL
-	echo -e "${Info} 已解封所有关键词 !"
+	echo -e "${Info} 모든 키워드를 사용 금지 해제하였습니다 !"
 }
 check_iptables(){
 	v4iptables=`iptables -V`
@@ -420,17 +420,17 @@ check_iptables(){
 			v6iptables="ip6tables"
 		fi
 	else
-		echo -e "${Error} 未安装 iptables 防火墙 !
-请安装 iptables防火墙：
-CentOS 系统：yum install iptables -y
-Debian / Ubuntu 系统：apt-get install iptables -y"
+		echo -e "${Error} iptables 방화벽이 설치되어 있지 않습니다 !
+iptables 방화벽을 설치해주세요：
+CentOS 시스템：yum install iptables -y
+Debian / Ubuntu 시스템：apt-get install iptables -y"
 	fi
 }
 Update_Shell(){
-	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ban_iptables.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
-	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 无法链接到 Github !" && exit 0
-	wget -N --no-check-certificate "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ban_iptables.sh" && chmod +x ban_iptables.sh
-	echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)" && exit 0
+	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/kikunae77/doubi/master/ban_iptables.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+	[[ -z ${sh_new_ver} ]] && echo -e "${Error} Github에 연결할 수 없습니다 !" && exit 0
+	wget -N --no-check-certificate "https://raw.githubusercontent.com/kikunae77/doubi/master/ban_iptables.sh" && chmod +x ban_iptables.sh
+	echo -e "스크립트가 최신 버전[ ${sh_new_ver} ]으로 업데이트 되었습니다!(주의：덮어씌우는 방식으로 업데이트 하므로, 아래에 오류 메시지가 표시될 수 있습니다. 아무것도 표시되지 않으면 정상입니다.)" && exit 0
 }
 check_sys
 check_iptables
@@ -443,27 +443,27 @@ if [[ ! -z $action ]]; then
 	[[ $action = "unbanspam" ]] && UnBan_SPAM && exit 0
 	[[ $action = "unbanall" ]] && UnBan_ALL && exit 0
 fi
-echo && echo -e " iptables防火墙 封禁管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
+echo && echo -e " iptables 방화벽 사용 금지 스크립트 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
   -- Toyo | doub.io/shell-jc2 --
 
-  ${Green_font_prefix}0.${Font_color_suffix} 查看 当前封禁列表
+  ${Green_font_prefix}0.${Font_color_suffix} 현재 금지 목록 확인
 ————————————
-  ${Green_font_prefix}1.${Font_color_suffix} 封禁 BT、PT
-  ${Green_font_prefix}2.${Font_color_suffix} 封禁 SPAM(垃圾邮件)
-  ${Green_font_prefix}3.${Font_color_suffix} 封禁 BT、PT+SPAM
-  ${Green_font_prefix}4.${Font_color_suffix} 封禁 自定义  端口
-  ${Green_font_prefix}5.${Font_color_suffix} 封禁 自定义关键词
+  ${Green_font_prefix}1.${Font_color_suffix} BT, PT 키워드 사용 금지
+  ${Green_font_prefix}2.${Font_color_suffix} SPAM(정크메일) 사용 금지
+  ${Green_font_prefix}3.${Font_color_suffix} BT, PT+SPAM 사용 금지
+  ${Green_font_prefix}4.${Font_color_suffix} 사용자 지정 포트 사용 금지
+  ${Green_font_prefix}5.${Font_color_suffix} 사용자 지정 키워드 사용 금지
 ————————————
-  ${Green_font_prefix}6.${Font_color_suffix} 解封 BT、PT
-  ${Green_font_prefix}7.${Font_color_suffix} 解封 SPAM(垃圾邮件)
-  ${Green_font_prefix}8.${Font_color_suffix} 解封 BT、PT+SPAM
-  ${Green_font_prefix}9.${Font_color_suffix} 解封 自定义  端口
- ${Green_font_prefix}10.${Font_color_suffix} 解封 自定义关键词
- ${Green_font_prefix}11.${Font_color_suffix} 解封 所有  关键词
+  ${Green_font_prefix}6.${Font_color_suffix} BT, PT 사용 금지 해제
+  ${Green_font_prefix}7.${Font_color_suffix} SPAM(정크메일) 사용 금지 해제
+  ${Green_font_prefix}8.${Font_color_suffix} BT, PT+SPAM 사용 금지 해제
+  ${Green_font_prefix}9.${Font_color_suffix} 사용자 지정 포트 사용 금지 해제
+ ${Green_font_prefix}10.${Font_color_suffix} 사용자 지정 키워드 사용 금지 해제
+ ${Green_font_prefix}11.${Font_color_suffix} 모든 키워드 사용 금지 해제
 ————————————
- ${Green_font_prefix}12.${Font_color_suffix} 升级脚本
+ ${Green_font_prefix}12.${Font_color_suffix} 스크립트 업데이트
 " && echo
-read -e -p " 请输入数字 [0-12]:" num
+read -e -p " 숫자를 입력하세요. [0-12]:" num
 case "$num" in
 	0)
 	View_ALL
@@ -505,6 +505,6 @@ case "$num" in
 	Update_Shell
 	;;
 	*)
-	echo "请输入正确数字 [0-12]"
+	echo "정확한 숫자를 입력해주세요. [0-12]"
 	;;
 esac
