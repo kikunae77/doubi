16951fe90437ada7abccb6982d7326e0fb8dbe25
kikunae77 <54462749+kikunae77@users.noreply.github.com>
2019-10-18 17:32:19 +0800

    번역

--- adbyby.sh
+++ adbyby.sh
@@ -15,9 +15,9 @@ adbyby_file="/usr/local/adbyby/bin/adbyby"
 adbyby_conf="/usr/local/adbyby/bin/adhook.ini"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
-Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+Info="${Green_font_prefix}[정보]${Font_color_suffix}"
+Error="${Red_font_prefix}[오류]${Font_color_suffix}"
+Tip="${Green_font_prefix}[주의]${Font_color_suffix}"
 
 #检查系统
 check_sys(){
@@ -39,7 +39,7 @@ check_sys(){
 	bit=`uname -m`
 }
 check_installed_status(){
-	[[ ! -e ${adbyby_file} ]] && echo -e "${Error} ADbyby 没有安装，请检查 !" && exit 1
+	[[ ! -e ${adbyby_file} ]] && echo -e "${Error} ADbyby 가 설치되어 있지 않습니다. 확인바랍니다 !" && exit 1
 }
 check_pid(){
 	PID=`ps -ef| grep "adbyby"| grep -v grep| grep -v "adbyby.sh"| grep -v "init.d"| grep -v "service"| awk '{print $2}'`
@@ -51,9 +51,9 @@ Download_adbyby(){
 	else
 		wget --no-check-certificate -O "adbyby.tar.gz" "https://raw.githubusercontent.com/adbyby/Files/master/linux.86.tar.gz"
 	fi
-	[[ ! -e "adbyby.tar.gz" ]] && echo -e "${Error} ADbyby 下载失败 !" && exit 1
+	[[ ! -e "adbyby.tar.gz" ]] && echo -e "${Error} ADbyby 다운로드 실패 !" && exit 1
 	tar -xzf adbyby.tar.gz && rm -rf adbyby.tar.gz
-	[[ ! -e "${adbyby_file}" ]] && echo -e "${Error} ADbyby 解压失败 !" && exit 1
+	[[ ! -e "${adbyby_file}" ]] && echo -e "${Error} ADbyby 압축해제 실패 !" && exit 1
 	cd bin
 	chmod 777 adbyby
 	wget -N --no-check-certificate "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/adhook.ini"
@@ -61,19 +61,19 @@ Download_adbyby(){
 Service_adbyby(){
 	if [[ ${release} = "centos" ]]; then
 		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/adbyby_centos -O /etc/init.d/adbyby; then
-			echo -e "${Error} ADbyby服务 管理脚本下载失败 !" && exit 1
+			echo -e "${Error} ADbyby 서비스 관리 스크립트 다운로드 실패 !" && exit 1
 		fi
 		chmod +x /etc/init.d/adbyby
 		chkconfig --add adbyby
 		chkconfig adbyby on
 	else
 		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/adbyby_debian -O /etc/init.d/adbyby; then
-			echo -e "${Error} ADbyby服务 管理脚本下载失败 !" && exit 1
+			echo -e "${Error} ADbyby 서비스 관리 스크립트 다운로드 실패 !" && exit 1
 		fi
 		chmod +x /etc/init.d/adbyby
 		update-rc.d -f adbyby defaults
 	fi
-	echo -e "${Info} ADbyby服务 管理脚本下载完成 !"
+	echo -e "${Info} ADbyby 서비스 관리 스크립트 다운로드 완료 !"
 }
 Installation_dependency(){
 	if [[ ${release} = "centos" ]]; then
@@ -86,33 +86,33 @@ Installation_dependency(){
 	mkdir ${file}
 }
 Install_adbyby(){
-	[[ -e ${adbyby_file} ]] && echo -e "${Error} 检测到 ADbyby 已安装 !" && exit 1
+	[[ -e ${adbyby_file} ]] && echo -e "${Error} ADbyby 이미 설치되어 있습니다 !" && exit 1
 	check_sys
-	echo -e "${Info} 开始安装/配置 依赖..."
+	echo -e "${Info} 설치 및 라이브러리 설정 시작..."
 	Installation_dependency
-	echo -e "${Info} 开始下载/安装..."
+	echo -e "${Info} 다운로드 및 설치 시작..."
 	Download_adbyby
-	echo -e "${Info} 开始下载/安装 服务脚本(init)..."
+	echo -e "${Info} 관리스크립트(init) 다운로드 및 설치 시작..."
 	Service_adbyby
-	echo -e "${Info} 开始设置 iptables防火墙..."
+	echo -e "${Info} iptables 방화벽 설정 시작..."
 	Set_iptables
-	echo -e "${Info} 开始添加 iptables防火墙规则..."
+	echo -e "${Info} iptables 방화벽 규칙 추가 시작..."
 	Add_iptables
-	echo -e "${Info} 开始保存 iptables防火墙规则..."
+	echo -e "${Info} iptables 방화벽 규칙 저장 시작..."
 	Save_iptables
-	echo -e "${Info} 所有步骤 安装完毕，开始启动..."
+	echo -e "${Info} 모든 절차가 완료되었습니다. adbyby를 시작합니다..."
 	Start_adbyby
 }
 Start_adbyby(){
 	check_installed_status
 	check_pid
-	[[ ! -z ${PID} ]] && echo -e "${Error} ADbyby 正在运行，请检查 !" && exit 1
+	[[ ! -z ${PID} ]] && echo -e "${Error} ADbyby 가 실행 중입니다. 확인 바랍니다 !" && exit 1
 	service adbyby start
 }
 Stop_adbyby(){
 	check_installed_status
 	check_pid
-	[[ -z ${PID} ]] && echo -e "${Error} ADbyby 没有运行，请检查 !" && exit 1
+	[[ -z ${PID} ]] && echo -e "${Error} ADbyby 실행중이지 않습니다. 확인 바랍니다 !" && exit 1
 	service adbyby stop
 }
 Restart_adbyby(){
@@ -128,9 +128,9 @@ Set_adbyby(){
 }
 Uninstall_adbyby(){
 	check_installed_status
-	echo "确定要卸载 ADbyby ? (y/N)"
+	echo "ADbyby 를 제거하시겠습니까 ? (y/N)"
 	echo
-	read -e -p "(默认: n):" unyn
+	read -e -p "(기본값: n):" unyn
 	[[ -z ${unyn} ]] && unyn="n"
 	if [[ ${unyn} == [Yy] ]]; then
 		check_pid
@@ -143,9 +143,9 @@ Uninstall_adbyby(){
 		else
 			update-rc.d -f adbyby remove
 		fi
-		echo && echo "ADbyby 卸载完成 !" && echo
+		echo && echo "ADbyby 제거 완료 !" && echo
 	else
-		echo && echo "卸载已取消..." && echo
+		echo && echo "제거 취소..." && echo
 	fi
 }
 Add_iptables(){
@@ -173,29 +173,29 @@ Set_iptables(){
 		chmod +x /etc/network/if-pre-up.d/iptables
 	fi
 }
-echo && echo -e "请输入一个数字来选择选项
+echo && echo -e "숫자를 입력해서 항목을 선택하세요.
 
- ${Green_font_prefix}1.${Font_color_suffix} 安装 ADbyby
- ${Green_font_prefix}2.${Font_color_suffix} 卸载 ADbyby
+ ${Green_font_prefix}1.${Font_color_suffix} ADbyby 설치
+ ${Green_font_prefix}2.${Font_color_suffix} ADbyby 제거
 ————————————
- ${Green_font_prefix}3.${Font_color_suffix} 启动 ADbyby
- ${Green_font_prefix}4.${Font_color_suffix} 停止 ADbyby
- ${Green_font_prefix}5.${Font_color_suffix} 重启 ADbyby
+ ${Green_font_prefix}3.${Font_color_suffix} ADbyby 시작
+ ${Green_font_prefix}4.${Font_color_suffix} ADbyby 정지
+ ${Green_font_prefix}5.${Font_color_suffix} ADbyby 재시작
 ————————————
- ${Green_font_prefix}6.${Font_color_suffix} 修改 配置文件
+ ${Green_font_prefix}6.${Font_color_suffix} 설정파일 수정
 ————————————" && echo
 if [[ -e ${adbyby_file} ]]; then
 	check_pid
 	if [[ ! -z "${PID}" ]]; then
-		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} 并 ${Green_font_prefix}已启动${Font_color_suffix}"
+		echo -e " 기존 상태: ${Green_font_prefix}이미 설치되어 있으며${Font_color_suffix} ${Green_font_prefix}실행 중입니다.${Font_color_suffix}"
 	else
-		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} 但 ${Red_font_prefix}未启动${Font_color_suffix}"
+		echo -e " 기존 상태: ${Green_font_prefix}이미 설치되어 있으나${Font_color_suffix} ${Red_font_prefix}실행 중이지 않습니다.${Font_color_suffix}"
 	fi
 else
-	echo -e " 当前状态: ${Red_font_prefix}未安装${Font_color_suffix}"
+	echo -e " 기존 상태: ${Red_font_prefix}설치되어 있지 않습니다.${Font_color_suffix}"
 fi
 echo
-read -e -p " 请输入数字 [1-8]:" num
+read -e -p " 숫자를 입력해주세요 [1-8]:" num
 case "$num" in
 	1)
 	Install_adbyby
@@ -216,6 +216,6 @@ case "$num" in
 	Set_adbyby
 	;;
 	*)
-	echo "请输入正确数字 [1-8]"
+	echo "정확한 숫자를 입력해 주세요 [1-8]"
 	;;
 esac
